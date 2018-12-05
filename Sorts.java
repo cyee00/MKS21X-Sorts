3a800e0d90e87072a42632259d9498760318f404
cyee00 <cyee00@stuy.edu>
2018-12-04 19:44:15 -0500

    w

--- Sorts.java
+++ Sorts.java
@@ -1,3 +1,16 @@
 public class Sorts{
-  public static void selectionsort(int [] ary) { }
+  public static void selectionsort(int [] ary) {
+    int x=ary[0];
+    int index=0;
+    for (int n=0;n<ary.length;n++){
+      for (int i=n;i<ary.length;i++){
+        if (ary[i]<ary[n-1]){
+          x=ary[i];
+          index=i;
+        }
+        ary[i]=ary[n];
+        ary[n]=x;
+      }
+    }
+  }
 }
