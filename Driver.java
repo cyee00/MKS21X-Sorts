535983c228de0bd37f2609ba116052aed804d9b8
cyee00 <cyee00@stuy.edu>
2018-12-04 20:38:51 -0500

    s

--- Driver.java
+++ Driver.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+
 public class Driver{
   public static void main(String[]args){
-    
+    int[] ary = new int[5];
+    /*for (int i=0;i<ary.length;i++){
+      ary[i]=Math.random();
+    }*/
+    ary[0]=23;
+    ary[1]=3;
+    ary[2]=2333;
+    ary[3]=231;
+    ary[4]=230;
+    System.out.println(java.util.Arrays.toString(ary));
+    System.out.println("\nTesting selectionSort");
+    System.out.println("\nselectionSort(ary) --> [1,2,3,4,5]");
+    Sorts.selectionSort(ary);
+    System.out.println(""+java.util.Arrays.toString(ary));
   }
 }
--- Sorts.java
+++ Sorts.java
@@ -1,16 +1,18 @@
 public class Sorts{
-  public static void selectionsort(int [] ary) {
+  public static void selectionSort(int [] ary) {
     int x=ary[0];
     int index=0;
     for (int n=0;n<ary.length;n++){
       for (int i=n;i<ary.length;i++){
-        if (ary[i]<ary[n-1]){
-          x=ary[i];
+        int smallest=ary[n];
+        if (ary[i]<=smallest){
+          smallest=ary[i];
           index=i;
         }
-        ary[i]=ary[n];
-        ary[n]=x;
       }
+      x=ary[index];
+      ary[index]=ary[n];
+      ary[n]=x;
     }
   }
 }
